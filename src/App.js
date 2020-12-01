c9de497cdc538fb226849508a99dd9f4c3ce4663
Abdul Khan <akhan17@outlook.com>
2020-12-01 17:36:52 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default class App extends Component {
   componentDidMount(){
       fetch('http://api.open-notify.org/astros.json')
       .then(response => {response.json()})
+      .then()
   }
 
   render() {
